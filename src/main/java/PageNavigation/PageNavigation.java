f7782f413273d4f4bb140616e32d6e2dac3f3741
DevMattyJS <matej.sliacky@protonmail.com>
2023-12-12 19:52:10 +0100

    Added PageNavigation class

--- src/main/java/PageNavigation/PageNavigation.java
+++ src/main/java/PageNavigation/PageNavigation.java
@@ -1,5 +1,26 @@
 package PageNavigation;
 
-public class PageNavigation {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class PageNavigation {
+	
+	public static void main(String[] args) {
+		
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
+		String baseUrl = "https://demo.nopcommerce.com/register";
+		String amazonUrl = "https://www.amazon.com";
+		
+		driver.get(baseUrl);
+		driver.get(amazonUrl);
+		driver.navigate().back();		// navigate one page backwards
+		driver.navigate().forward();	// navigate one page forward
+		driver.navigate().refresh();	// refresh the actual page
+		
+		
+	}
 }
